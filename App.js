ef3b960dc2ea1ef8a6dca63a871d907ad46b2296
dmonzonis <dmonzonis@protonmail.com>
2021-03-02 21:45:02 +0100

    Allow adding stuff to a list of items

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, ScrollView } from 'react-native';
+
+const ItemTextInput = (props) => {
+    return (
+        <View style={styles.itemTextInputArea}>
+            <TextInput style={styles.itemTextInput}
+                placeholder="Enter text here"
+                onChangeText={props.changeTextHandler} />
+        </View>
+    );
+}
+
+const ListItem = (props) => {
+    return (
+        <Text style={styles.listItem}>{props.itemText}</Text>
+    );
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [inputText, setInputText] = useState("");
+    const [items, setItems] = useState([]);
+
+    const changeTextHandler = (text) => {
+        setInputText(text);
+    };
+
+    const addItemHandler = () => {
+        setItems(items => [...items, inputText]);
+    };
+
+    return (
+        <View style={styles.root}>
+            <View style={styles.inputArea}>
+                <ItemTextInput changeTextHandler={changeTextHandler} />
+                <Button title="ADD" onPress={addItemHandler} />
+            </View>
+
+            <ScrollView>
+                {items.map((item, idx) => <ListItem key={idx} itemText={item} />)}
+            </ScrollView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    root: {
+        paddingTop: 60,
+        paddingHorizontal: 20,
+    },
+    inputArea: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignContent: "center",
+        marginBottom: 20,
+    },
+    itemTextInputArea: {
+        width: "80%",
+        borderColor: "black",
+        borderWidth: 1,
+        borderRadius: 5,
+    },
+    itemTextInput: {
+        marginHorizontal: 10,
+    },
+    listItem: {
+        paddingHorizontal: 15,
+        marginVertical: 7,
+    }
 });
